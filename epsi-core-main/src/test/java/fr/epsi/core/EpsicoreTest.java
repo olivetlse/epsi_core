bad8e6fc9b1e0c000905d3f1ae04b3e6451276e2
olivetlse <91941729+olivetlse@users.noreply.github.com>
2022-01-24 13:53:57 +0100

    Add files via upload

---
+++ epsi-core-main/src/test/java/fr/epsi/core/EpsicoreTest.java
@@ -0,0 +1,14 @@
+package fr.epsi.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EpsicoreTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ epsi-core-main/src/test/java/fr/epsi/core/StringUtilsTests.java
@@ -0,0 +1,49 @@
+package fr.epsi.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StringUtilsTests {
+
+	@Test
+	void test() {
+
+	}
+
+	@ParameterizedTest(name = "{0} est un palindrome")
+	@ValueSource(strings = { "ete", "a but tuba" })
+
+	void isPalindrome(String sentense) {
+		assertTrue(StringUtils.isPalindrome(sentense));
+
+	}
+
+	@ParameterizedTest(name = "{0} n est pas un palindrome")
+	@ValueSource(strings = { "ile", "je code" })
+
+	void isNotPalindrome(String sentense) {
+		assertFalse(StringUtils.isPalindrome(sentense));
+
+	}
+
+	@ParameterizedTest(name = "{0} encodee :{1}  ")
+	@CsvFileSource(resources = "/cesar.csv", numLinesToSkip = 1)
+
+	void cesarEncode(String sentence, String request, int step) {
+		assertEquals(request, StringUtils.cesarEncode(sentence, step));
+
+	}
+
+	@ParameterizedTest(name = "{1} decodee donne :{0}  ")
+	@CsvFileSource(resources = "/cesar.csv", numLinesToSkip = 1)
+
+	void cesarDecode(String sentence, String request, int step) {
+		assertEquals(sentence, StringUtils.cesarDecode(request, step));
+	}
+}
